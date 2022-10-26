e3e9eb63a154cf5a63bcdf87c8daa713f9332465
bloodykatanna <115177542+bloodykatanna@users.noreply.github.com>
2022-10-26 19:21:26 +0100

    Add files via upload

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,35 +6,37 @@
 /*   By: azaghlou <azaghlou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 13:16:50 by azaghlou          #+#    #+#             */
-/*   Updated: 2022/10/19 17:56:39 by azaghlou         ###   ########.fr       */
+/*   Updated: 2022/10/23 21:26:48 by azaghlou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-int ft_atoi(const char *str)
-{
-    int a = 0;
-    int res = 0;
-    int sign = 1;
-    
-    while (str[a] == 32 || (str[a] >= 9 && str[a] <= 13) )
-        a++;
-    
-    if (str[a] == '-' )
-    {
-         sign*= -1;
-         a++;
-    }   
-    else if(str[a] == '+')
-    {
-        sign = 1;
-        a++;
-    }
-    while ((str[a] >= '0' && str[a] <= '9' ) && str[a])
-    {
-        res = (res * 10) + str[a] - '0';
-        a++;
-    }
-    return (res * sign);
-}
 
+int	ft_atoi(const char *str)
+{
+	int	a;
+	int	res;
+	int	sign;
+
+	a = 0;
+	res = 0;
+	sign = 1;
+	while (str[a] == 32 || (str[a] >= 9 && str[a] <= 13))
+		a++;
+	if (str[a] == '-')
+	{
+		sign *= -1;
+		a++;
+	}
+	else if (str[a] == '+')
+	{
+		sign = 1;
+		a++;
+	}
+	while ((str[a] >= '0' && str[a] <= '9') && str[a])
+	{
+		res = (res * 10) + str[a] - '0';
+		a++;
+	}
+	return (res * sign);
+}
